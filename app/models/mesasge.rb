c13faaec299dc9f4499551b93819609e705ca70d
shun_sugimoto <shun.sugimoto.kaisya@gmail.com>
2016-03-08 08:39:59 +0000

    using bootstrap

--- app/models/mesasge.rb
+++
@@ -1,2 +0,0 @@
-class Mesasge < ActiveRecord::Base
-end
--- test/models/mesasge_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MesasgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
